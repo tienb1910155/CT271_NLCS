ac0350dc1a3817bb7c6aed5690ee147389b8f315
tienb1910155 <thanhtien123lx@gmail.com>
2022-09-30 21:55:28 +0700

    Lần sửa 16

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\Users\LoginController;
+use App\Http\Controllers\Admin\MainController;
+use App\Http\Controllers\Admin\MenuController;
+
+Route::get('admin/users/login', [LoginController::class,'index'])->name('login');
+Route::post('admin/users/login/store', [LoginController::class,'store']);
+
+Route::middleware(['auth'])->group(function() {
+
+    Route::prefix('admin')->group(function() {
+        Route::get('/',[MainController::class,'index'])->name('admin');
+        Route::get('main',[MainController::class,'index']);
+
+        #menu
+        Route::prefix('menus')->group(function(){
+            Route::get('add',[MenuController::class,'create'])->name('admin.menus.add');
+            Route::post('add',[MenuController::class,'store']);
+            Route::get('list',[MenuController::class,'index']);
+            Route::get('edit/{menu}',[MenuController::class,'show']);
+            Route::post('edit/{menu}',[MenuController::class,'update']);
+            Route::DELETE('destroy', [MenuController::class, 'destroy']);
+        });
+    });
+});
+
